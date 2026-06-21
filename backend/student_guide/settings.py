83ef49a979130f0d53837f10622465aaa55ab368
ahmedamara020-max <ahmedamara020@gmail.com>
2026-06-21 17:55:22 +0300

    b3b3

--- backend/student_guide/settings.py
+++ backend/student_guide/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 from pathlib import Path
 import dj_database_url
+from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,6 +121,12 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+}
+from datetime import timedelta
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1), 
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
